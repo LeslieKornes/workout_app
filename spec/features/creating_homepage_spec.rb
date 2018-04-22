c7af3243605d8e7cc56bb60b71cfb1690b163ed1
Leslie Kornes <lesliekornes@gmail.com>
2018-04-22 01:58:20 -0400

    Create home page spec.

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "Creating Homepage" do
+
+  scenario "User visits homepage" do
+    visit '/'
+
+    expect(page).to have_link("Home")
+    expect(page).to have_link("Athlete's Den")
+    expect(page).to have_content("Workout Lounge!")
+    expect(page).to have_content("Show off your workout")
+  end
+
+end
