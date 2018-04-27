91da4579c6f162e9405e49e87d0f9b402bd2c119
Leslie Kornes <lesliekornes@gmail.com>
2018-04-26 22:46:15 -0400

    Modify spec to only show workouts within the past 7 days in Lounge.

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -12,6 +12,10 @@ RSpec.feature "Listing exercises" do
     @e2 = @john.exercises.create(duration_in_min: 40,
                                   workout: "It's cardio time!",
                                   workout_date: 2.days.ago)
+
+    @e3 = @john.exercises.create(duration_in_min: 55,
+                                  workout: "Beach body video",
+                                  workout_date: 8.days.ago)
   end
 
   scenario "shows user's workout for past 7 days" do
@@ -26,6 +30,10 @@ RSpec.feature "Listing exercises" do
     expect(page).to have_content(@e2.duration_in_min)
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
+
+    expect(page).not_to have_content(@e3.duration_in_min)
+    expect(page).not_to have_content(@e3.workout)
+    expect(page).not_to have_content(@e3.workout_date)
   end
 
   scenario "shows no exercises if none created" do
