ece01b89e8819b389e42cdf0fbbeaa284d398484
Leslie Kornes <lesliekornes@gmail.com>
2018-04-26 23:17:53 -0400

    Add editing exercise spec.

---
+++ spec/features/exercises/editing_exercise_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Editing exercise" do
+  before do
+    @owner = User.create(email: "owner@example.com", password: "password")
+
+    @owner_exercise = @owner.exercises.create(duration_in_min: 48,
+                                  workout: "My body building workout",
+                                  workout_date: Date.today)
+
+    login_as(@owner)
+  end
+
+  scenario "shows no exercises if none created" do
+
+    visit "/"
+
+    click_link "My Lounge"
+
+    path = "/users/#{@owner.id}/exercises/#{@owner_exercise.id}/edit"
+    link = "a[href=\'#{path}\]"
+    find(link).click
+
+    fill_in "Duration", with: 45
+    click_button "Update Exercise"
+
+    expect(page).to have_content("Exercise has been updated")
+    expect(page).to have_content(45)
+    expect(page).not_to have_content(48)
+  end
+
+end
