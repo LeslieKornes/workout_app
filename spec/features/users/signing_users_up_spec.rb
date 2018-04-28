023be963d1faa53f61e3b4e7088ba978dfe52b6b
Leslie Kornes <lesliekornes@gmail.com>
2018-04-28 16:14:15 -0400

    Modify signing users up spec.

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -5,18 +5,19 @@ RSpec.feature "Users signup" do
   scenario "with valid credentials" do
 
     visit "/"
-
     click_link "Sign up"
 
+    fill_in "First Name", with: "John"
+    fill_in "Last Name",  with: "Doe"
     fill_in "Email", with: "john@example.com"
-
     fill_in "Password",  with: "password"
-
     fill_in "Password confirmation",  with: "password"
-
     click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully.")
+
+    visit "/"
+    expect(page).to have_content("John Doe")
 
   end
 end
